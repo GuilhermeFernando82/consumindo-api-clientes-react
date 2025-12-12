5a9539802853f18b7f114ab9a06ea34889d9b060
guilh <dotdark82@gmail.com>
2025-12-12 12:20:02 -0300

    ajuste rota

--- pages/index.js
+++ pages/index.js
@@ -119,22 +119,6 @@ export default function App() {
 
   return (
     <div className={styles.pageContainer}>
-      <Head>
-        {/* --- OPEN GRAPH TAGS --- */}
-        <meta property="og:title" content="Sistema de Clientes" />
-        <meta property="og:description" content="Gerenciamento de clientes." />
-        <meta
-          property="og:image"
-          content="https://cdls.org.br/wp-content/uploads/cdlce_base/2020/07/iStock-1223604108.jpg"
-        />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-
-        {/* Para Facebook reconhecer */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://SEU-SITE-AQUI.com" />
-      </Head>
       <motion.div
         variants={containerVariants}
         initial="hidden"
